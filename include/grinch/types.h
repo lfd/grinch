1b61f86fcc7050071d86a935b12a450051437689
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-06-23 20:26:38 +0200

    types: introduce more primitive types
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/grinch/types.h
+++ include/grinch/types.h
@@ -35,6 +35,16 @@ typedef short s16;
 typedef int s32;
 typedef char s8;
 
+typedef unsigned long long __u64;
+typedef unsigned short __u16;
+typedef unsigned int __u32;
+typedef unsigned char __u8;
+
+typedef long long __s64;
+typedef short __s16;
+typedef int __s32;
+typedef char __s8;
+
 typedef unsigned long size_t;
 typedef u64 uintptr_t;
 typedef s64 intptr_t;
