7ba9327263ea38dcda0cc47e84505f0096d33219
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-06-25 23:12:02 +0200

    grinch: add stddef.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ include/grinch/stddef.h
@@ -0,0 +1,18 @@
+/*
+ * Grinch, a minimalist operating system
+ *
+ * Copyright (c) OTH Regensburg, 2023
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+#define offsetof(TYPE, MEMBER)	__builtin_offsetof(TYPE, MEMBER)
+
+#endif /* _STDDEF_H */
