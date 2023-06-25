8046c1cfdbb1caadbeb2a8e09a37fad8ea7f721f
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-06-25 23:11:53 +0200

    grinch: add container_of.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ include/grinch/container_of.h
@@ -0,0 +1,28 @@
+/*
+ * Grinch, a minimalist operating system
+ *
+ * Copyright (c) OTH Regensburg, 2023
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+/* Partly copied from the Linux kernel */
+
+#ifndef _CONTAINER_OF_H
+#define _CONTAINER_OF_H
+
+#include <grinch/compiler_types.h>
+#include <grinch/stddef.h>
+
+#define container_of(ptr, type, member) ({				\
+	void *__mptr = (void *)(ptr);					\
+	static_assert(__same_type(*(ptr), ((type *)0)->member) ||	\
+		      __same_type(*(ptr), void),			\
+		      "pointer type mismatch in container_of()");	\
+	((type *)(__mptr - offsetof(type, member))); })
+
+#endif /* _CONTAINER_OF_H */
