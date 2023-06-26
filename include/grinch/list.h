b713dbcb9ced952c462e97760ecc398fda18c2c8
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-06-26 16:22:09 +0200

    list: improve lists
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/grinch/list.h
+++ include/grinch/list.h
@@ -19,6 +19,11 @@
 
 #include <grinch/container_of.h>
 
+#define POISON_POINTER_DELTA	0
+
+#define LIST_POISON1  ((void *) 0x100 + POISON_POINTER_DELTA)
+#define LIST_POISON2  ((void *) 0x122 + POISON_POINTER_DELTA)
+
 #define list_entry(ptr, type, member) \
 	container_of(ptr, type, member)
 
@@ -47,6 +52,17 @@ static inline void __list_add(struct list_head *new,
 	WRITE_ONCE(prev->next, new);
 }
 
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	WRITE_ONCE(prev->next, next);
+}
+
+static inline void __list_del_entry(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+}
+
 static inline void list_add(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head, head->next);
@@ -57,7 +73,19 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head)
 	__list_add(new, head->prev, head);
 }
 
+static inline void list_del(struct list_head *entry)
+{
+	__list_del_entry(entry);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
+}
+
 #define list_for_each(pos, head) \
 	for (pos = (head)->next; !list_is_head(pos, (head)); pos = pos->next)
+
+#define list_for_each_safe(pos, n, head)	\
+	for (pos = (head)->next, n = pos->next;	\
+	     !list_is_head(pos, (head));	\
+	     pos = n, n = pos->next)
 
 #endif /* _LIST_H */
