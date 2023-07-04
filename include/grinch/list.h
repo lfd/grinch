8af5020771dc70e4ebb3770092007b28cd50d0db
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-05 00:05:04 +0200

    list: improve lists
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/grinch/list.h
+++ include/grinch/list.h
@@ -31,6 +31,11 @@ struct list_head {
 	struct list_head *next, *prev;
 };
 
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+
 static inline void INIT_LIST_HEAD(struct list_head *list)
 {
 	WRITE_ONCE(list->next, list);
