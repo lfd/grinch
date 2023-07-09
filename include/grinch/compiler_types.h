edf7fa98ffb28e408d6c2549f7f1b68bb4ba2873
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-09 23:51:10 +0200

    improve compiler_types.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/grinch/compiler_types.h
+++ include/grinch/compiler_types.h
@@ -20,4 +20,18 @@
 #define __static_assert(expr, msg, ...) _Static_assert(expr, msg)
 #define static_assert(expr, ...) __static_assert(expr, ##__VA_ARGS__, #expr)
 
+#define __scalar_type_to_expr_cases(type)				\
+		unsigned type:	(unsigned type)0,			\
+		signed type:	(signed type)0
+
+#define __unqual_scalar_typeof(x) typeof(				\
+		_Generic((x),						\
+			 char:	(char)0,				\
+			 __scalar_type_to_expr_cases(char),		\
+			 __scalar_type_to_expr_cases(short),		\
+			 __scalar_type_to_expr_cases(int),		\
+			 __scalar_type_to_expr_cases(long),		\
+			 __scalar_type_to_expr_cases(long long),	\
+			 default: (x)))
+
 #endif /* _COMPILER_TYPES_H */
