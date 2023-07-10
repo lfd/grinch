cc0844125441553e8cbf63c061ee827a13bebd2b
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-11 01:21:46 +0200

    Makefile: user: add more abstraction
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- user/apps/init/inc.mk
+++ user/apps/init/inc.mk
@@ -1,14 +1,1 @@
-APP=init
-
-APP_OBJS=main.o
-
-user/apps/$(APP)/built-in.a: $(LIBC_BUILTIN) user/apps/$(APP)/main.o
-
-user/apps/$(APP)/$(APP)_linked.o: user/apps/$(APP)/built-in.a
-	$(call ld_user)
-
-user/apps/$(APP)/$(APP).echse: user.ld user/apps/$(APP)/$(APP)_linked.o
-	$(call ld_app_user)
-
-clean_init:
-	rm -rf user/apps/$(APP)/*.{o,a,echse}
+INIT_OBJS=user/apps/init/main.o
--- user/inc.mk
+++ user/inc.mk
@@ -18,19 +18,36 @@ user/%.o: user/%.S $(GENERATED)
 	$(VERBOSE) $(CC) -c $(AFLAGS_USER) $(CFLAGS_USER) -o $@ $<
 
 define ld_app_user
-	$(QUIET) "[LD-APP]$@"
-	$(VERBOSE) $(LD) $(LDFLAGS_USER) --gc-sections -T $^ -o $@
+	$(QUIET) "[LD-APP]$(1)"
+	$(VERBOSE) $(LD) $(LDFLAGS_USER) --gc-sections -T $(2) -o $(1)
 	$(if $(V), $(SZ) --format=SysV -x $@)
 endef
 
 define ld_user
-	$(QUIET) "[LD-U]  $@"
-	$(VERBOSE) $(LD) $(LDFLAGS_USER) --whole-archive -relocatable -o $@ $<
+	$(QUIET) "[LD-U]  $(1)"
+	$(VERBOSE) $(LD) $(LDFLAGS_USER) --whole-archive -relocatable -o $(1) $(2)
 endef
 
 APPS=init
 
-include $(patsubst %,%/inc.mk,user/apps/$(APPS))
+include $(patsubst %,user/apps/%/inc.mk,$(APPS))
+
+UC = $(shell echo '$1' | tr '[:lower:]' '[:upper:]')
+
+define define_app
+clean_$(1):
+	rm -rf user/apps/$(1)/*.{o,a,echse}
+
+user/apps/$(1)/built-in.a: $(LIBC_BUILTIN) $($(call UC,$(1))_OBJS)
+
+user/apps/$(1)/$(1)_linked.o: user/apps/$(1)/built-in.a
+	$(call ld_user,$$@,$$^)
+
+user/apps/$(1)/$(1).echse: user.ld user/apps/$(1)/$(1)_linked.o
+	$(call ld_app_user,$$@,$$^)
+endef
+
+$(foreach app,$(APPS),$(eval $(call define_app,$(app))))
 
 clean_user: $(patsubst %,clean_%,$(APPS))
 	rm -rf user/*.{elf,o,a}
