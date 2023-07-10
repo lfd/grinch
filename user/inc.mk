d79ee3bff26f4737b91445307972b40c4c209635
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-10 23:11:58 +0200

    userspace: improve makefile
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- user/inc.mk
+++ user/inc.mk
@@ -2,6 +2,11 @@ INCLUDES_USER = -Iuser/include -Iinclude_common -Iuser/lib/$(ARCH)/include
 
 CFLAGS_USER = $(CFLAGS_COMMON) $(CFLAGS_ARCH) $(INCLUDES_USER)
 
+LIBC_OBJS = user/lib/stdio.o user/lib/string.o user/lib/unistd.o user/lib/sched.o
+LIBC_OBJS += user/lib/$(ARCH)/entry.o
+
+user/lib/built-in.a: $(LIBC_OBJS)
+
 user/%.o: user/%.c
 	$(QUIET) "[CC-U]  $@"
 	$(VERBOSE) $(CC) -c $(CFLAGS_USER) -o $@ $<
@@ -10,27 +15,26 @@ user/%.o: user/%.S $(GENERATED)
 	$(QUIET) "[AS-U]  $@"
 	$(VERBOSE) $(CC) -c $(AFLAGS_USER) $(CFLAGS_USER) -o $@ $<
 
-user/user.o: user/built-in.a
+define ld_app_user
+	$(QUIET) "[LD-APP]$@"
+	$(VERBOSE) $(LD) $(LDFLAGS_USER) --gc-sections -T $^ -o $@
+	$(if $(V), $(SZ) --format=SysV -x $@)
+endef
+
+define ld_user
 	$(QUIET) "[LD-U]  $@"
 	$(VERBOSE) $(LD) $(LDFLAGS_USER) --whole-archive -relocatable -o $@ $<
-
-user/user.elf: user.ld user/user.o
-	$(QUIET) "[LD-U]  $@"
-	$(VERBOSE) $(LD) $(LDFLAGS_USER) --gc-sections -T $^ -o $@
-ifdef V
-	$(SZ) --format=SysV -x $@
-endif
-
-LIBC = user/lib/stdio.o user/lib/string.o user/lib/unistd.o user/lib/sched.o
-LIBC += user/lib/$(ARCH)/entry.o
-
-user/lib/built-in.a: $(LIBC)
+endef
 
 user/built-in.a: user/lib/built-in.a user/main.o
 
-user/user.bin: user/user.elf
+user/user.o: user/built-in.a
+	$(call ld_user)
+
+user/user.elf: user.ld user/user.o
+	$(call ld_app_user)
 
 clean_user:
-	rm -rf user/*.{elf,o,a,bin}
+	rm -rf user/*.{elf,o,a}
 	rm -rf user/lib/*.{o,a}
 	rm -rf user/lib/$(ARCH)/*.{o,a}
