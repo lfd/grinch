e254221ef6914a845cb01e1d9f1f33d16b75cc94
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-12 00:09:16 +0200

    grinch: add ctype.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ include/grinch/ctype.h
@@ -0,0 +1,38 @@
+/*
+ * Grinch, a minimalist operating system
+ *
+ * Copyright (c) OTH Regensburg, 2023
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+/* Copied from the Linux kernel */
+
+#ifndef _CTYPE_H
+#define _CTYPE_H
+
+#define _U	0x01	/* upper */
+#define _L	0x02	/* lower */
+#define _D	0x04	/* digit */
+#define _C	0x08	/* cntrl */
+#define _P	0x10	/* punct */
+#define _S	0x20	/* white space (space/lf/tab) */
+#define _X	0x40	/* hex digit */
+#define _SP	0x80	/* hard space (0x20) */
+
+extern const unsigned char _ctype[];
+
+#define __ismask(x) (_ctype[(int)(unsigned char)(x)])
+
+#define isxdigit(c)	((__ismask(c)&(_D|_X)) != 0)
+
+static inline char _tolower(const char c)
+{
+	return c | 0x20;
+}
+
+#endif /* _CTYPE_H */
