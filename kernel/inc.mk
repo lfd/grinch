a7c429ad6f067b40414c38a3297c43b3e045073f
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-07-12 01:31:02 +0200

    core: introduce user.S
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- kernel/inc.mk
+++ kernel/inc.mk
@@ -1,5 +1,7 @@
-KERNEL_OBJS = main.o syscall.o task.o uaccess.o
+KERNEL_OBJS = main.o syscall.o task.o uaccess.o user.o
 
 KERNEL_OBJS := $(addprefix kernel/, $(KERNEL_OBJS))
 
 kernel/built-in.a: $(KERNEL_OBJS)
+
+kernel/user.o: user/apps/init/init.echse
