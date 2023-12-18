c98246260ff89d873cc959975db3edfee7a5b135
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2023-12-18 14:12:42 +0100

    riscv: shrink qemu down to 64MiB
    That must suffice.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- arch/riscv/inc.mk
+++ arch/riscv/inc.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE ?= riscv64-linux-gnu-
 
 QEMU=qemu-system-riscv64
-QEMU_ARGS=-m 128M -smp 2 -serial stdio -nographic -monitor telnet:127.0.0.1:55555,server,nowait -machine virt -cpu rv64,h=true
+QEMU_ARGS=-m 64M -smp 2 -serial stdio -nographic -monitor telnet:127.0.0.1:55555,server,nowait -machine virt -cpu rv64,h=true
 
 CFLAGS_ARCH = -mcmodel=medany -march=rv64imafdc_zifencei -DARCH_RISCV=1
 LDFLAGS_ARCH = -melf64lriscv
