11f996a49e6024aaacea4151a3626469ba108ea3
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-02-19 18:26:42 +0100

    riscv: add helper rule to obtain a dump of Qemu's DT
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- arch/riscv/inc.mk
+++ arch/riscv/inc.mk
@@ -3,7 +3,8 @@ CROSS_COMPILE ?= riscv64-linux-gnu-
 UBOOT_CFG=riscv-qemu.config
 
 QEMU=qemu-system-riscv64
-QEMU_ARGS=-m 64M -smp 2 -serial stdio -nographic -monitor telnet:127.0.0.1:55555,server,nowait -machine virt -cpu rv64,h=true
+QEMU_MACHINE=-machine virt
+QEMU_ARGS=-m 64M -smp 2 -serial stdio -nographic -monitor telnet:127.0.0.1:55555,server,nowait $(QEMU_MACHINE) -cpu rv64,h=true
 QEMU_UBOOT_ARGS=\
 		-device loader,file=kernel.bin,addr=0x82000000,force-raw=on \
 		-device loader,file=user/initrd.cpio,addr=0x82800000,force-raw=on \
@@ -19,6 +20,11 @@ ARCH_OBJS += vmm/vmm.o vmm/vmm_ecall.o
 ARCH_OBJS := $(addprefix $(ARCH_DIR)/, $(ARCH_OBJS))
 
 arch/riscv/built-in.a: $(ARCH_OBJS)
+
+qemu.dts:
+	$(QEMU_CMD_DIRECT) $(QEMU_MACHINE),dumpdtb=/tmp/qemu_tmp.dtb
+	dtc -I dtb -O dts /tmp/qemu_tmp.dtb -o $@
+	rm -f /tmp/qemu_tmp.dtb
 
 clean_arch:
 	$(RMRF) $(ARCH_DIR)/*.{o,a}
