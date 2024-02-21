3123491595abbc65b69f765eac38db3580a61bf3
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-02-21 21:29:02 +0100

    common: move errno.h to common
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- common/include/grinch/errno.h
+++ common/include/grinch/errno.h
@@ -1,7 +1,7 @@
 /*
  * Grinch, a minimalist operating system
  *
- * Copyright (c) OTH Regensburg, 2022
+ * Copyright (c) OTH Regensburg, 2022-2024
  *
  * Authors:
  *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
