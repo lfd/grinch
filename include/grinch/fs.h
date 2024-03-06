5b60ca6aa6a42fa3074b7fa18607fc96897371bd
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-03-07 00:50:21 +0100

    fs: add central fs.h header
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

---
+++ include/grinch/fs.h
@@ -0,0 +1,58 @@
+/*
+ * Grinch, a minimalist operating system
+ *
+ * Copyright (c) OTH Regensburg, 2024
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _FS_H
+#define _FS_H
+
+#include <grinch/types.h>
+
+struct file;
+struct file_system;
+
+struct stat {
+	loff_t size;
+};
+
+struct fs_flags {
+	unsigned char may_read:1;
+	unsigned char may_write:1;
+	unsigned char is_kernel:1;
+};
+
+struct file_handle {
+	struct file *fp;
+	struct fs_flags flags;
+	loff_t position;
+};
+
+struct file_operations {
+	ssize_t (*read)(struct file_handle *, char *ubuf, size_t count);
+	ssize_t (*write)(struct file_handle *, const char *ubuf, size_t count);
+	void (*close)(struct file *);
+	int (*stat)(struct file *, struct stat *st);
+};
+
+struct file {
+	const struct file_operations *fops;
+	void *drvdata;
+};
+
+struct file_system_operations {
+	int (*open_file)(const struct file_system *fs, struct file *filep,
+			 const char *fname, struct fs_flags flags);
+};
+
+struct file_system {
+	const struct file_system_operations *fs_ops;
+};
+
+#endif /* _FS_H */
