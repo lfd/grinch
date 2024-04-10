fc9e8a3a5887b6bd1efb11803c285b2053e37ad3
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-04-10 15:01:42 +0200

    align: improve align.h
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/grinch/align.h
+++ include/grinch/align.h
@@ -15,14 +15,19 @@
 #ifndef _ALIGN_H
 #define _ALIGN_H
 
-#define ALIGN_MASK(x, mask)	(((x) + (mask)) & ~(mask))
-#define ALIGN(x, a)		ALIGN_MASK(x, (__typeof__(x))(a) - 1)
-#define ALIGN_DOWN(x, a)	ALIGN((x) - ((a) - 1), (a))
+#define ALIGN_MASK(x, mask)		(((x) + (mask)) & ~(mask))
 
-#define PTR_ALIGN(p, a)		((typeof(p))ALIGN((unsigned long)(p), (a)))
-#define PTR_ALIGN_DOWN(p, a)	((typeof(p))ALIGN_DOWN((unsigned long)(p), (a)))
+#define IS_ALIGNED(x, a)		(((x) & ((typeof(x))(a) - 1)) == 0)
+#define ALIGN(x, a)			ALIGN_MASK(x, (__typeof__(x))(a) - 1)
+#define ALIGN_DOWN(x, a)		ALIGN((x) - ((a) - 1), (a))
+#define PAGE_ALIGN_DOWN(addr)		ALIGN_DOWN(addr, PAGE_SIZE)
+#define PAGE_ALIGNED(addr)		IS_ALIGNED(addr, PAGE_SIZE)
 
-#define PTR_IS_ALIGNED(x, a)	(IS_ALIGNED((unsigned long)(x), (a)))
-#define IS_ALIGNED(x, a)	(((x) & ((typeof(x))(a) - 1)) == 0)
+#define PTR_IS_ALIGNED(x, a)		(IS_ALIGNED((unsigned long)(x), (a)))
+#define PTR_ALIGN(p, a)			((typeof(p))ALIGN((unsigned long)(p), (a)))
+#define PTR_ALIGN_DOWN(p, a)		((typeof(p))ALIGN_DOWN((unsigned long)(p), (a)))
+#define PTR_PAGE_ALIGN_DOWN(addr)	PTR_ALIGN_DOWN(addr, PAGE_SIZE)
+#define PTR_PAGE_ALIGNED(addr)		PTR_IS_ALIGNED((unsigned long)(addr), PAGE_SIZE)
+
 
 #endif /* _ALIGN_H */
