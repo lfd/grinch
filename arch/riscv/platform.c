lfd/grinch

commit 82a45c0c49cd5f8d4c4a27db1a0ce19bda469108

platform, smp: initialise percpu structures earlier
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

arch/riscv/platform.c
@@ -23,6 +23,7 @@ int __init arch_platform_init(void)
 {
 	const char *name, *isa;
 	unsigned long hart_id;
+	struct per_cpu *pcpu;
 	int err, off, child;
 	const fdt32_t *reg;
 
@@ -66,10 +67,12 @@ int __init arch_platform_init(void)
 
 		pri("%s: HART %lu available\n", name, hart_id);
 
-		err = phys_mark_used(v2p(per_cpu(hart_id)),
-				     PAGES(sizeof(struct per_cpu)));
+		pcpu = per_cpu(hart_id);
+		err = phys_mark_used(v2p(pcpu), PAGES(sizeof(*pcpu)));
 		if (err)
-			return err;
+			return trace_error(err);
+
+		memset(pcpu, 0, sizeof(*pcpu));
 	}
 
 	return 0;

arch/riscv/smp.c
@@ -74,9 +74,6 @@ int arch_boot_cpu(unsigned long hart_id)
 	pr("Bringing up HART %lu\n", hart_id);
 	pcpu = per_cpu(hart_id);
 
-	/* Zero everything */
-	memset(pcpu, 0, sizeof(struct per_cpu));
-
 	pcpu->cpuid = hart_id;
 	spin_init(&pcpu->remote_call.lock);
 

commit 5f335ee6c6e38ecd6c83e49d1cdb65af67be87bc

riscv: platform: Undo initialisation of percpu structures
1. They are already zeroed by the loader
2. Hell, this was a mistake: we overwrite percpu structures of the boot
   CPU. I don't know why this didn't crash earlier. I saw that it was a
   mistake by testing the kernel with -O2.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Fixes: 82a45c0c ("platform, smp: initialise percpu structures earlier")

arch/riscv/platform.c
@@ -73,8 +73,6 @@ int __init arch_platform_init(void)
 		if (err)
 			return trace_error(err);
 
-		memset(pcpu, 0, sizeof(*pcpu));
-
 		ic = fdt_subnode_offset(_fdt, child,
 					ISTR("interrupt-controller"));
 		if (ic < 0)