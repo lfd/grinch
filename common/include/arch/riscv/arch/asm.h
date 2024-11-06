3e793ea7356d0a63e534b5c99d64add41224de53
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-11-06 17:13:32 +0100

    core: rv32: asm: introduce rv32 bits
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- common/include/arch/riscv/arch/asm.h
+++ common/include/arch/riscv/arch/asm.h
@@ -14,6 +14,10 @@
 #define REG_S	sd /* 64-Bit Double Load */
 #define REG_L	ld /* 32-Bit Double Load */
 #define SZREG	8 /* Register Size: 8 Byte / 64 Bit */
+#elif ARCH_RISCV == 32 /* rv32 */
+#define REG_S	sw /* 32-Bit Word Store */
+#define REG_L	lw /* 32-Bit Word Load */
+#define SZREG	4 /* Register Size: 4 Byte / 32 Bit */
 #else
 #error "Unknown RISC-V Architecture"
 #endif
