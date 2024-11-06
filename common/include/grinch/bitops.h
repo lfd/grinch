40f3d1fc5be9d6a931a60244de8acb33fd13372c
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2024-11-06 22:24:54 +0100

    bitops: implement fsl stuff
    Copy from the Linux kernel.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- common/include/grinch/bitops.h
+++ common/include/grinch/bitops.h
@@ -10,8 +10,57 @@
  * the COPYING file in the top-level directory.
  */
 
+/* Copied and adapted from the Linux Kernel Sources */
+
 #include <grinch/types.h>
 #include <grinch/compiler_attributes.h>
+
+/* generic_fls from the Linux kernel */
+static __always_inline int fls(unsigned int x)
+{
+	int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xffff0000u)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xff000000u)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xf0000000u)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xc0000000u)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000u)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+#if BITS_PER_LONG == 32
+static __always_inline int fls64(__u64 x)
+{
+	__u32 h = x >> 32;
+	if (h)
+		return fls(h) + 32;
+	return fls(x);
+}
+
+static inline unsigned int fls_long(unsigned long l)
+{
+	if (sizeof(l) == 4)
+		return fls(l);
+	return fls64(l);
+}
+#endif
 
 static __always_inline int __ffsl(unsigned long word)
 {
