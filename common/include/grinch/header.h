6fd0ca1efe1a97b51765ebddde11f1a979578a81
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2025-10-09 02:00:04 +0200

    core: header: use nonstring attribute
    In order to compile with C23.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- common/include/grinch/header.h
+++ common/include/grinch/header.h
@@ -1,7 +1,7 @@
 #define GRINCH_SIGNATURE	":GRINCH:"
 
 struct grinch_header {
-	char signature[8];
+	char signature[8] __attribute__((nonstring));
 	union {
 		u64 __gcov_info_head;
 		void *gcov_info_head;
