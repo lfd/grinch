d85d1a2037f0c49a1c6df6e5a3fe549d03eb6def
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2026-07-03 13:59:03 +0200

    grinch_layout.h: prepare for arm64
    rv64 and arm64 share the same virtual layout; consolidate them into a
    single definition block.  Preserve the rv32 section so all future three
    architectures compile correctly.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/asm-generic/grinch_layout.h
+++ include/asm-generic/grinch_layout.h
@@ -10,21 +10,27 @@
  * the COPYING file in the top-level directory.
  */
 
-/* Check if this applies for ARM64 */
 #define USER_START		_UL(0x1000)
-
-#define USER_STACK_SIZE		(1 * MIB)
-#define USER_STACK_TOP		USER_END
-#define USER_STACK_BOTTOM	(USER_STACK_TOP - USER_STACK_SIZE)
 
 /* Must be a multiple of 256 KiB */
 #define GRINCH_SIZE	(8 * 256 * KIB)
 
-/* Generic definitions */
 #define GRINCH_END	(GRINCH_BASE + GRINCH_SIZE)
 #define IOREMAP_END	(IOREMAP_BASE + IOREMAP_SIZE)
 
-#if ARCH_RISCV == 64 /* rv64 */
+#if defined(ARCH_RISCV) && ARCH_RISCV == 32 /* rv32 */
+#define GRINCH_BASE	_UL(0xd8000000)
+#define USER_END	IOREMAP_BASE
+#define IOREMAP_BASE	_UL(0xc0000000)
+#define IOREMAP_SIZE	((256 + 128) * MIB)
+
+#define PERCPU_BASE	GRINCH_END
+#define KHEAP_BASE      (GRINCH_END + (32 * 64) * KIB)
+
+/* The uppermost 512 MiB belong to the direct mapping */
+#define DIR_PHYS_BASE	_UL(0xe0000000)
+
+#else /* rv64, arm64 */
 /*
  * On RV64 we have at least SV39 for paging. On SV39, this is the uppermost
  * address that comes before GRINCH_BASE
@@ -37,20 +43,13 @@
 #define IOREMAP_SIZE	(512 * MIB)
 #define KHEAP_BASE      (IOREMAP_END)
 
-#define USER_END		(_UL(1) << (39 - 1))
+#define USER_END	(_UL(1) << (39 - 1))
 
-#elif ARCH_RISCV == 32 /* rv32 */
-#define GRINCH_BASE	_UL(0xd8000000)
-#define USER_END	IOREMAP_BASE
-#define IOREMAP_BASE	_UL(0xc0000000)
-#define IOREMAP_SIZE	((256 + 128) * MIB)
-
-#define PERCPU_BASE	GRINCH_END
-#define KHEAP_BASE      (GRINCH_END + (32 * 64) * KIB)
-
-/* The uppermost 512 MiB belong to the direct mapping */
-#define DIR_PHYS_BASE	_UL(0xe0000000)
 #endif
+
+#define USER_STACK_SIZE		(1 * MIB)
+#define USER_STACK_TOP		USER_END
+#define USER_STACK_BOTTOM	(USER_STACK_TOP - USER_STACK_SIZE)
 
 #ifndef __ASSEMBLY__
 
