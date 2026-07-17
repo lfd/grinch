2ff2131dbcbd39293faf4f7684eae278f0335318
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2026-07-17 23:52:40 +0200

    layout: drop the include guard from grinch_layout.h again
    Since the previous commit, asm/percpu.h no longer pulls in the layout
    definitions itself - grinch_layout.h is back to a single includer per
    translation unit, so the guard has no user left. Undo it.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- include/asm-generic/grinch_layout.h
+++ include/asm-generic/grinch_layout.h
@@ -10,9 +10,6 @@
  * the COPYING file in the top-level directory.
  */
 
-#ifndef _GRINCH_LAYOUT_H
-#define _GRINCH_LAYOUT_H
-
 #define USER_START		_UL(0x1000)
 
 /* Must be a multiple of 256 KiB */
@@ -60,5 +57,3 @@ static inline unsigned char *grinch_base(void)
 }
 
 #endif /* __ASSEMBLY__ */
-
-#endif /* _GRINCH_LAYOUT_H */
