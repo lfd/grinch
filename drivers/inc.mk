591d31a8cc934605b92534f97ba93abfb514ea7a
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2026-08-13 11:46:39 +0200

    drivers: make irqchips and SBI console conditionally compileable
    The interrupt controllers and the SBI console were tied to the
    architecture by hand.  Select them through their config symbols instead,
    the way the UART drivers already are, so that the architecture dependency
    lives in the declarations where it is expressed once.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

--- drivers/inc.mk
+++ drivers/inc.mk
@@ -1,30 +1,26 @@
-DRIVERS_OBJS = device.o
-DRIVERS_OBJS += driver.o
-DRIVERS_OBJS += fb/bochs.o
-DRIVERS_OBJS += fb/host.o
-DRIVERS_OBJS += pci/pci.o
+DRIVERS_OBJS-y := device.o
+DRIVERS_OBJS-y += driver.o
+DRIVERS_OBJS-y += fb/bochs.o
+DRIVERS_OBJS-y += fb/host.o
+DRIVERS_OBJS-y += pci/pci.o
+DRIVERS_OBJS-$(CONFIG_SBI_TTY) += sbi-tty.o
+DRIVERS_OBJS := $(DRIVERS_OBJS-y)
 
-DRIVERS_IRQ_OBJS = irq/irqchip.o
+DRIVERS_IRQ_OBJS-y := irq/irqchip.o
+DRIVERS_IRQ_OBJS-$(CONFIG_IRQ_APLIC) += irq/riscv/aplic.o
+DRIVERS_IRQ_OBJS-$(CONFIG_IRQ_PLIC) += irq/riscv/plic.o
+DRIVERS_IRQ_OBJS-$(CONFIG_IRQ_XPLIC) += irq/riscv/xplic.o
+DRIVERS_IRQ_OBJS-$(CONFIG_IRQ_GIC_V2) += irq/gic-v2.o
+DRIVERS_IRQ_OBJS := $(DRIVERS_IRQ_OBJS-y)
 
-DRIVERS_SERIAL_OBJS-y  := serial/chardev.o serial/serial.o
-DRIVERS_SERIAL_OBJS-$(CONFIG_UART_8250)    += serial/uart-8250.o
+DRIVERS_SERIAL_OBJS-y := serial/chardev.o
+DRIVERS_SERIAL_OBJS-y += serial/serial.o
+DRIVERS_SERIAL_OBJS-$(CONFIG_UART_8250) += serial/uart-8250.o
 DRIVERS_SERIAL_OBJS-$(CONFIG_UART_APBUART) += serial/uart-apbuart.o
-DRIVERS_SERIAL_OBJS-$(CONFIG_UART_LITEUART)+= serial/uart-liteuart.o
-DRIVERS_SERIAL_OBJS-$(CONFIG_UART_UARTLITE)+= serial/uart-uartlite.o
-DRIVERS_SERIAL_OBJS-$(CONFIG_UART_PL011)   += serial/uart-pl011.o
+DRIVERS_SERIAL_OBJS-$(CONFIG_UART_LITEUART) += serial/uart-liteuart.o
+DRIVERS_SERIAL_OBJS-$(CONFIG_UART_UARTLITE) += serial/uart-uartlite.o
+DRIVERS_SERIAL_OBJS-$(CONFIG_UART_PL011) += serial/uart-pl011.o
 DRIVERS_SERIAL_OBJS := $(DRIVERS_SERIAL_OBJS-y)
-
-DRIVERS_TIMER_OBJS =
-ifdef ARCH_RISCV
-	DRIVERS_OBJS += sbi-tty.o
-
-	DRIVERS_IRQ_OBJS += irq/riscv/aplic.o
-	DRIVERS_IRQ_OBJS += irq/riscv/plic.o
-	DRIVERS_IRQ_OBJS += irq/riscv/xplic.o
-endif
-ifdef ARCH_ARM64
-	DRIVERS_IRQ_OBJS += irq/gic-v2.o
-endif
 
 DRIVERS_OBJS := $(addprefix drivers/, $(DRIVERS_OBJS))
 DRIVERS_IRQ_OBJS := $(addprefix drivers/, $(DRIVERS_IRQ_OBJS))
